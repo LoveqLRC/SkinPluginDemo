f3c7cbea963271fd2aca5a4898913e4a2dd62cea
LoveqLRC <664215432@qq.com>
2018-02-13 10:32:15 +0800

    解决加载插件皮肤出错的问题，添加注释和皮肤文件

--- app/src/main/java/rc/loveq/skinplugindemo/MainActivity.java
+++ app/src/main/java/rc/loveq/skinplugindemo/MainActivity.java
@@ -24,16 +24,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void changeSkin(View view) throws Exception {
         Resources nativeResources = getResources();
+        //创建AssetManager，但是不能直接new所以只能通过反射
         AssetManager assetManager = AssetManager.class.newInstance();
+        // 反射获取addAssetPath方法
         Method method = AssetManager.class.getDeclaredMethod("addAssetPath", String.class);
-        method.invoke(assetManager, Environment.getExternalStorageDirectory().getAbsolutePath()
+        // 皮肤包的路径:  本地sdcard/plugin.skin
+        // 反射调用addAssetPath方法
+        method.invoke(assetManager, Environment.getExternalStorageDirectory().getAbsoluteFile()
                 + File.separator + "plugin.skin");
+        // 创建皮肤的Resources对象
         Resources pluginResources = new Resources(assetManager,
                 nativeResources.getDisplayMetrics(),
                 nativeResources.getConfiguration());
-
-        int drawableId = pluginResources.getIdentifier("ic_launcher", "drawable",
+        // 通过资源名称,类型，包名获取Id
+        int drawableId = pluginResources.getIdentifier("plugin_img", "drawable",
                 "rc.loveq.skinplugin");
+        // 设置背景
         mIvImage.setImageDrawable(pluginResources.getDrawable(drawableId));
     }
 }
